2d3d513a1a82e95556de8164a38b6a878fffc990
CarolinaLujan23 <69494895+CarolinaLujan23@users.noreply.github.com>
2021-05-21 17:49:24 -0300

    Add files via upload

--- vaquitas.py
+++ vaquitas.py
@@ -14,9 +14,27 @@ class Vaca(threading.Thread):
         self.posicion = 0
         self.velocidad = random.uniform(0.1, 0.9)
 
+#?????
+#        semaforoVaca.acquire()
+
+#        try:
+#            while self.posicion > inicioPuente:
+#                time.sleep (1 - self.velocidad)
+#                self.posicion += 1
+#                semaforoVaca.release()    
+#        finally:
+#            semaforoVaca.release()    
+
+
     def avanzar(self):
         time.sleep(1-self.velocidad)
         self.posicion += 1
+        if self.posicion == inicioPuente:
+            semaforoVaca.acquire()
+            while self.posicion < (largoPuente+inicioPuente):
+                time.sleep(1-self.velocidad)
+                self.posicion += 1
+            semaforoVaca.release()   
 
     def dibujar(self):
         print(' ' * self.posicion + '🐮') # si no funciona, cambiá por 'V'
@@ -24,8 +42,20 @@ class Vaca(threading.Thread):
     def run(self):
         while(True):
             self.avanzar()
+#            while self.posicion == inicioPuente:
+#                semaforoVaca.acquire()
+#                self.avanzar()
+#            while self.posicion > largoPuente:
+#                semaforoVaca.release()
+#            if self.posicion == 50:
+#                self.posicion = 0
+                
+#Se traban(?) las vacas y no sé cómo arreglarlo.                
+                    
+
 
 vacas = []
+semaforoVaca = threading.Semaphore(1)
 for i in range(cantVacas):
     v = Vaca()
     vacas.append(v)
